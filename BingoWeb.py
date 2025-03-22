07b110fe97723fa15389ddddaed56eb6a0096797
sko9370 <28380215+sko9370@users.noreply.github.com>
2025-03-22 17:04:54 -0400

    save progress, but switch from table to label inside card because of live update headache

--- BingoWeb.py
+++ BingoWeb.py
@@ -12,6 +12,7 @@ class BingoWeb:
 
     def start_game(self):
         ui.markdown('# BINGO!')
+
         # Winning pattern
         with ui.row(align_items='center'):
             ui.label('Winning Pattern: ').classes('text-xl')
@@ -19,11 +20,47 @@ class BingoWeb:
                 ui.label().classes('text-xl').bind_text_from(
                     self.model_dict, 'pattern'
                 )
+
+        # History Table
+        with ui.row(align_items='center'):
+            bingo_rows = [
+                {'letter': 'B'},
+                {'letter': 'I'},
+                {'letter': 'N'},
+                {'letter': 'G'},
+                {'letter': 'O'},
+            ]
+            history_table = ui.table(rows=bingo_rows).props('table-header-class=hidden')
+
+            rows = [{}, {}, {}, {}, {}]
+            for i in range(5):
+                row_dict = rows[i]
+                for j in range(1, 16):
+                    row_dict[j] = (i*15) + j
+            history_table = ui.table(
+                rows=rows,
+                row_key='b',
+                on_select=lambda p: self.model_dict.update({'pattern':p.selection[0]['pattern']}) if p.selection else False
+            ).props('table-header-class=hidden')
+
+            array = list(self.model.picked_numbers)
+            history_table.add_slot('body-cell', f'''
+                <q-td :props="props">
+                    <q-badge :color="{array}.includes(props.value) ? 'green' : 'blue'">
+            '''+
+            '''
+                        {{ props.value }}
+                    </q-badge>
+                </q-td>
+            '''
+            )
+
         # Last picked number
         with ui.row(align_items='center'):
             ui.label('Last Picked: ').classes('text-xl')
             with ui.card():
                 ui.label().classes('text-xl').bind_text_from(self.model_dict, 'last')
+        #with ui.row(align_items='center'):
             ui.space()
             # Last 3 picked
             ui.label('Last 3: ').classes('text-xl')
@@ -32,32 +69,8 @@ class BingoWeb:
                     self.model_dict, 'recent',
                     backward=lambda r: r
                 )
-        # History (hidden by default)
+        # History
         show_history = {'value': False}
-
-        '''
-        columns = [{
-            'name': 'pattern',
-            'label': 'Set Winning Pattern',
-            'field': 'pattern',
-            'required': True,
-            'sortable': True
-        }]
-        rows = [
-            {'pattern': '5 IN A ROW'},
-            {'pattern': '4 CORNERS'},
-            {'pattern': 'PYRAMID'}
-        ]
-        pattern_table = ui.table(
-            columns=columns,
-            rows=rows,
-            row_key='pattern',
-            selection='single',
-            on_select=lambda p: self.model_dict.update({'pattern':p.selection[0]['pattern']}) if p.selection else False
-        )
-        pattern_table.selected = [{'pattern': '5 IN A ROW'}]
-        '''
-        #ui.table().bind_visibility_from(show_history, 'value')
 
         with ui.row(align_items='center').bind_visibility_from(
             show_history, 'value'
@@ -71,7 +84,7 @@ class BingoWeb:
 
         # Buttons
         with ui.row():
-            ui.button('Pick next', on_click=lambda: self.pick_number())
+            ui.button('Pick next', on_click=lambda: self.pick_number(history_table))
             ui.button('Restart', on_click=lambda: self.restart())
         
 
@@ -108,29 +121,45 @@ class BingoWeb:
                 {'pattern': '4 CORNERS'},
                 {'pattern': 'PYRAMID'}
             ]
-            pattern_table = ui.table(
+            with ui.table(
                 columns=columns,
                 rows=rows,
                 row_key='pattern',
                 selection='single',
                 on_select=lambda p: self.model_dict.update({'pattern':p.selection[0]['pattern']}) if p.selection else False
-            )
-            pattern_table.selected = [{'pattern': '5 IN A ROW'}]
-
-            with ui.row(align_items='center'):
-                pattern = ui.input(
-                    label='Set Custom Pattern',
-                    placeholder='Pattern'
-                )
-                ui.button(
-                    'Add Custom Pattern',
-                    on_click=lambda: pattern_table.add_row(
-                        {'pattern': str.upper(pattern.value)}
-                    )
-                )
+            ) as pattern_table:
+                pattern_table.selected = [{'pattern': '5 IN A ROW'}]
+                with pattern_table.add_slot('bottom-row'):
+                    with pattern_table.row():
+                        with pattern_table.cell():
+                            ui.button(on_click=lambda: (
+                                pattern_table.add_row(
+                                    {'pattern': str.upper(pattern.value)}
+                                ), pattern.set_value(None)
+                            ), icon='add').props('flat fab-mini')
+                        with pattern_table.cell():
+                            pattern = ui.input(
+                                label='Add Custom Pattern',
+                                placeholder='Pattern'
+                            )
 
         # Run web
         ui.run()
+    
+    def refresh_table(self, history_table):
+        array = [int(num) for num in list(self.model.picked_numbers)]
+        print(f'refreshing with {array}')
+        history_table.add_slot('body-cell', f'''
+            <q-td :props="props">
+                <q-badge :color="{array}.includes(props.value) ? 'green' : 'blue'">
+        '''+
+        '''
+                    {{ props.value }}
+                </q-badge>
+            </q-td>
+        '''
+        )
+
     
     def check_bingo(self, bingo_picks):
         self.model_dict['bingo'] = str(self.model.check_bingo(bingo_picks))
@@ -147,9 +176,10 @@ class BingoWeb:
                 ui.button('Close', on_click=dialog.close)
                 ui.space()
     
-    def pick_number(self):
+    def pick_number(self, history_table):
         self.model.pick_number()
         self.refresh_dict()
+        self.refresh_table(history_table)
     
     def restart(self):
         self.model.reset_board()
